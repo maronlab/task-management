feaa8abe4e0b85e56a1d4078288c1521b7375131
maronlab <maronsergeant777@gmail.com>
2026-08-13 22:06:17 +0900

    タスク受け渡し処理追加

--- src/main/java/com/github/maronlab/task_app/controller/TaskController.java
+++ src/main/java/com/github/maronlab/task_app/controller/TaskController.java
@@ -1,13 +1,25 @@
 package com.github.maronlab.task_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.github.maronlab.task_app.data.TaskDto;
 
 @Controller
 public class TaskController {
-    
+
     @GetMapping("/task")
-    private String display(){
+    private String display() {
         return "/index.html";
+    }
+
+    @PostMapping("/task")
+    public String showTask(@ModelAttribute TaskDto dto, Model model) {
+
+        model.addAttribute("task", dto);
+
+        return "/task-result";
     }
 }
---
+++ src/main/java/com/github/maronlab/task_app/data/TaskDto.java
@@ -0,0 +1,42 @@
+package com.github.maronlab.task_app.data;
+
+import java.time.LocalDate;
+
+public class TaskDto {
+    private String taskName;
+    private LocalDate period;
+    private String priority;
+    private String status;
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public LocalDate getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(LocalDate period) {
+        this.period = period;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
